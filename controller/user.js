ca9988b0f817b011a5ba8b98e68130a47424a0c4
ljy <m18318859136@163.com>
2026-05-23 20:23:00 +0800

    查重接口完成+登录接口起步

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,4 @@
-import { registerModel } from '../model/user.js'
+import { registerModel, queryUserNameModel } from '../model/user.js'
 
 // 用户注册
 export const register = async (req, res, next) => {
@@ -13,6 +13,33 @@ export const register = async (req, res, next) => {
         user_name,
       }
     })
+  } catch (err) {
+    next(err)
+  }
+}
+
+// 用户名查重
+export const checkUsername = async (req, res, next) => {
+  try {
+    const { user_name } = req.query
+    const result = await queryUserNameModel(user_name)
+
+    res.json({
+      code: 200,
+      message: '用户名查重成功',
+      data: {
+        user_name,
+        isExist: result,
+      }
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const login = async (req, res, next) => {
+  try {
+
   } catch (err) {
     next(err)
   }
--- router/user.js
+++ router/user.js
@@ -1,10 +1,16 @@
 import express from 'express'
 const router = express.Router()
 
-import { register } from '../controller/user.js'
+import { register, checkUsername } from '../controller/user.js'
 import { registerValidator } from '../validator/user.js'
 
 // 用户注册
 router.post('/register', registerValidator, register)
+
+// 用户名查重
+router.get('/register/checkUsername', checkUsername)
+
+// 用户登录
+router.post('/login', login)
 
 export default router
