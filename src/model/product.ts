11fce11c2db0b129b11b0355750fd94881a41990
ljy <m18318859136@163.com>
2026-06-01 14:28:08 +0800

    同步 数据库新增的软删除字段

--- src/model/product.ts
+++ src/model/product.ts
@@ -4,7 +4,7 @@ import pool from '../config/db.ts'
 
 // 分页获取商品信息
 export const productInfoModel = async (offset: number, limit: number) => {
-  const sql = 'SELECT m_id, product_name, product_type, product_num, product_description FROM t_product ORDER BY m_id ASC LIMIT ?, ?'
+  const sql = 'SELECT m_id, product_name, product_type, product_num, product_description FROM t_product WHERE is_delete = 0 ORDER BY m_id ASC LIMIT ?, ?'
   const [res] = await pool.query<RowDataPacket[]>(sql, [offset, limit])
   return res
 }
